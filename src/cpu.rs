c65e81fc3adf87b2f18ad0c3b059f4284c3af350
nmandrus1 <nmandrus1@gmail.com>
2023-09-15 22:59:07 -0400

    Experimenting with Load Instruction design

--- src/cpu.rs
+++ src/cpu.rs
@@ -81,12 +81,38 @@ impl CPU {
     }
 }
 
-// impl Default for CPU {
-//     fn default() -> Self {}
-// }
+/// Address type
+// check out the "newtype" pattern in Rust to see more examples
+// Doing this rather than just using a u16 provides a few benefits
+// 1) Type saftey: instead of some vague u16 floating around in
+//     the the type will make it clear what the value represents
+// 2) Readability: This makes the code very readable and at a glance
+//     it becomes clear based on the type alone what the variable does
+// 3) Flexibilty: We can define methods on this type and even change its
+//     representation and behavior if we need to in the future
+struct Address(u16);
 
 /// Enum to represent the different possible operands for a Load instruction
-enum LoadTarget {
+// Things to keep in mind:
+// 1) Errors: Not every combination of registers may be allowed,
+//     maybe creating a special LoadRegister that only contains valid
+//     registers?
+enum LoadOperand {
+    // Read or Write to/from a register
     Reg(Register),
-    Mem(u16),
+
+    // Read or Write to/from a memory address
+    Mem(Address),
+}
+
+/// Struct to contain the source and destination of a Load Instruction
+struct LoadOp {
+    src: LoadOperand,
+    dest: LoadOperand,
+
+    // Some Load Instructions will do more than just load, they will
+    // decrement the value located at the previously written to address,
+    // in cases like these we can represent that as an Optional followup
+    // function to be run after the Operation
+    followup: Option<fn(Address)>,
 }
